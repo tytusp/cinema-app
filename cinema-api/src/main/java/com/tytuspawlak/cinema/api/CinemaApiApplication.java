531ac403026dd7a8916bebc971ad69c81f914ecc
Tytus Pawlak <tytus.pawlak@gmail.com>
2021-11-13 20:57:23 +0100

    init modules

---
+++ cinema-api/src/main/java/com/tytuspawlak/cinema/api/CinemaApiApplication.java
@@ -0,0 +1,12 @@
+package com.tytuspawlak.cinema.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApiApplication.class, args);
+	}
+}
