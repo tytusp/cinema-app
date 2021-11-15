1bb7249c9060ef208e94b94987d5bc29ec0ceec0
Tytus Pawlak <tytus.pawlak@gmail.com>
2021-11-15 02:55:18 +0100

    bug fix for searching for movie by ID

--- cinema-api/src/main/java/com/tytuspawlak/cinema/api/controller/movie/MovieSearchByIdStrategyProvider.java
+++ cinema-api/src/main/java/com/tytuspawlak/cinema/api/controller/movie/MovieSearchByIdStrategyProvider.java
@@ -6,6 +6,7 @@ import com.tytuspawlak.cinema.core.service.movie.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
@@ -13,10 +14,10 @@ import java.util.function.Function;
 @Component
 @RequiredArgsConstructor
 class MovieSearchByIdStrategyProvider {
-    private static final Map<MovieIdType, Function<MovieService, Function<String, Optional<MovieDTO>>>> STRATEGIES_BY_ID_TYPE = Map.of(
+    private static final Map<MovieIdType, Function<MovieService, Function<String, Optional<MovieDTO>>>> STRATEGIES_BY_ID_TYPE = new HashMap<>(Map.of(
             MovieIdType.IMDB, service -> service::findMovieByImdbId,
             MovieIdType.LOCAL_DB, service -> service::findMovieById
-    );
+    ));
     private final MovieService service;
 
 
